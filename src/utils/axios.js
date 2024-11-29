d3a723b38c6cb0522a290afe4ad9d03d20f402cc
beslicn <nikola_beslic@yahoo.com>
2024-11-29 17:15:45 +0100

    edit env files

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASEPATH,
 });
 
 axios.defaults.withCredentials = false;

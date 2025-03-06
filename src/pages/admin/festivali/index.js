6e9d6821d75334b5b0a583b66d50df57b2d5b9b0
beslicn <nikola_beslic@yahoo.com>
2025-03-06 21:20:11 +0100

    git react hupkast add + adminsidebar etc FIX

--- src/pages/admin/festivali/index.js
+++ src/pages/admin/festivali/index.js
@@ -1,13 +1,11 @@
-import AdminNav from "../../../components/admin/header/AdminNav";
+import AdminNav from "../../../components/admin/layout/AdminNav";
 
 export default function FestivaliPage() {
-
     return (
         <>
             <AdminNav />
             <div className="container">
                 <p>Ovde dodje lista festivala</p>
-
             </div>
         </>
     );

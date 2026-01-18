acc452f9fe769b2ed099b882902306b036076630
beslicn <nikola_beslic@yahoo.com>
2026-01-18 13:27:41 +0100

    git react admin naslovna tekstovi na slajderu

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,19 +1,31 @@
 import {
+    Backdrop,
     Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
     Checkbox,
+    CircularProgress,
+    IconButton,
     Paper,
+    Stack,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
+    Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import moment from "moment";
 import axiosClient from "../../utils/axios";
 import EditNoteIcon from "@mui/icons-material/EditNote";
+import PlaylistRemoveIcon from "@mui/icons-material/PlaylistRemove";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function AdminHomePage() {
     const [tekstovi, setTekstovi] = useState([]);
@@ -21,12 +33,17 @@ export default function AdminHomePage() {
     const [komentari, setKomentari] = useState([]);
     const [festivali, setFestivali] = useState([]);
     const [igranja, setIgranja] = useState([]);
+    const [loading, setLoading] = useState(false);
     const router = useRouter();
 
     useEffect(() => {
+        setLoading(true);
         axiosClient
             .get("/admin/tekstovi-za-naslovnu")
-            .then((res) => setTekstovi(res.data))
+            .then((res) => {
+                console.log(res.data);
+                setTekstovi(res.data);
+            })
             .catch((err) => console.error(err));
 
         axiosClient
@@ -47,7 +64,8 @@ export default function AdminHomePage() {
         axiosClient
             .get("/admin/igranja-za-naslovnu")
             .then((res) => setIgranja(res.data))
-            .catch((err) => console.error(err));
+            .catch((err) => console.error(err))
+            .finally(() => setLoading(false));
     }, []);
 
     const handleEditClick = (tekstid) => {
@@ -55,8 +73,40 @@ export default function AdminHomePage() {
         router.push(`/admin/tekstovi/create?tekstid=${tekstid}`);
     };
 
+    const handleRemoveFromSlider = (tekstid) => {
+        setLoading(true);
+        axiosClient
+            .put("/admin/tekstovi/ukloni-sa-slajdera", { tekstid })
+            .then((res) => {
+                setTekstovi((prevTekstovi) =>
+                    prevTekstovi.map((tekst) =>
+                        tekst.tekstid === tekstid
+                            ? { ...tekst, na_slajderu: 0 }
+                            : tekst
+                    )
+                );
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
     return (
         <>
+            {loading && (
+                <Backdrop
+                    open={loading}
+                    sx={{
+                        color: "#fff",
+                        zIndex: (theme) => theme.zIndex.drawer + 1,
+                    }}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+            )}
             <h1>HuP Admin</h1>
             <p>
                 TO DO / IDEAS za naslovnu: najnoviji komentari za odobrenje,
@@ -71,6 +121,64 @@ export default function AdminHomePage() {
             >
                 Dodaj tekst
             </Button>
+            <h2>Tekstovi na slajderu</h2>
+            <Stack direction="row" spacing={2} sx={{ mb: 4 }}>
+                {tekstovi
+                    .filter((t) => t.na_slajderu == 1)
+                    .map((tekst) => (
+                        <Card sx={{ maxWidth: 300 }}>
+                            <CardMedia
+                                sx={{ height: 180 }}
+                                image={tekst.tekst_photo}
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="div"
+                                >
+                                    {tekst.naslov}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <IconButton
+                                    aria-label="edit tekst"
+                                    color="primary"
+                                    title="Edituj tekst"
+                                    component={Link}
+                                    href={`/admin/tekstovi/edit?tekstid=${tekst.tekstid}`}
+                                    prefetch
+                                >
+                                    <EditNoteIcon />
+                                </IconButton>
+                                <IconButton
+                                    aria-label="Kopiraj link"
+                                    color="primary"
+                                    title="Kopiraj link"
+                                    onClick={() => {
+                                        navigator.clipboard.writeText(
+                                            window.location.origin +
+                                                `/tekst/${tekst.slug}`
+                                        );
+                                    }}
+                                >
+                                    <ContentCopyIcon />
+                                </IconButton>
+                                <IconButton
+                                    aria-label="remove from slider"
+                                    color="error"
+                                    title="Ukloni sa slajdera"
+                                    onClick={() =>
+                                        handleRemoveFromSlider(tekst.tekstid)
+                                    }
+                                >
+                                    <PlaylistRemoveIcon />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    ))}
+            </Stack>
+
             <h2>10 najnovijih tekstova</h2>
             <TableContainer component={Paper} sx={{ mb: 4 }}>
                 <Table size="small">
@@ -116,14 +224,15 @@ export default function AdminHomePage() {
             </TableContainer>
 
             <h2>10 najnovijih predstava</h2>
-
             <TableContainer component={Paper} sx={{ mb: 4 }}>
                 <Table size="small">
                     <TableHead>
-                        <TableCell>Naziv predstave</TableCell>
-                        <TableCell>Pozorista</TableCell>
-                        <TableCell>Datum premijere</TableCell>
-                        <TableCell>Akcije</TableCell>
+                        <TableRow>
+                            <TableCell>Naziv predstave</TableCell>
+                            <TableCell>Pozorista</TableCell>
+                            <TableCell>Datum premijere</TableCell>
+                            <TableCell>Akcije</TableCell>
+                        </TableRow>
                     </TableHead>
                     <TableBody>
                         {predstave.map((pred) => (

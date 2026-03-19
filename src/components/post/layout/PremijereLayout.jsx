4b149ab754b63fd316dffc38d354641f725b6057
beslicn <nikola_beslic@yahoo.com>
2026-03-19 23:59:17 +0100

    git react premijere layout

--- src/components/post/layout/PremijereLayout.jsx
+++ src/components/post/layout/PremijereLayout.jsx
@@ -5,29 +5,32 @@ import Link from "next/link";
 const PremijereLayout = ({ data, pClass, videoIcon }) => {
     return (
         <div
-            className={`media post-block post-block__small predstava-index-wrapper ${
+            className={`media post-block post-block__small predstava-index-wrapper premijera-index-wrapper bg-grey-light-three ${
                 pClass ?? "post-block__on-dark-bg m-b-xs-30"
             }`}
         >
-            <Link
-                href={`/predstave/${data.predstava_slug}`}
-                className="align-self-center"
-            >
-                <>
-                    <Image
-                        src={data.plakat || "/slike/vizitke-cover.jpg"}
-                        alt={data.predstava_slug}
-                        width={80}
-                        height={120}
-                        objectFit="cover"
-                    />
-                    {videoIcon === true ? (
-                        <span className="video-play-btn video-play-btn__small" />
-                    ) : (
-                        ""
-                    )}
-                </>
-            </Link>
+            <div className="premijera-index-image-wrapper">
+                <Link
+                    href={`/predstave/${data.predstava_slug}`}
+                    className="align-self-center"
+                >
+                    <>
+                        <Image
+                            src={data.plakat || "/slike/vizitke-cover.jpg"}
+                            alt={data.predstava_slug}
+                            width={80}
+                            height={120}
+                            objectFit="cover"
+                            className="premijere-index-image"
+                        />
+                        {videoIcon === true ? (
+                            <span className="video-play-btn video-play-btn__small" />
+                        ) : (
+                            ""
+                        )}
+                    </>
+                </Link>
+            </div>
 
             <div className="media-body predstava-index-info">
                 {/* {data.zanrovi?.map(zanr => (
@@ -40,19 +43,29 @@ const PremijereLayout = ({ data, pClass, videoIcon }) => {
                         </a>
                     ))} */}
 
-                <h3 className="axil-post-title hover-line hover-line predstava-index-title">
+                <h4 className="axil-post-title hover-line hover-line">
                     <Link href={`/predstave/${data.predstava_slug}`}>
                         {data.naziv_predstave}
                     </Link>
-                </h3>
+                </h4>
                 <div className="post-metas">
                     <div className="post-cat-group premijere-naziv-pozorista">
-                        {data.pozorista.map((poz) => (
-                            <span key={poz.pozoristeid} className="text-muted">
-                                {" "}
-                                {poz.naziv_pozorista}
-                            </span>
-                        ))}
+                        {data.pozorista.length > 0 && (
+                            <>
+                                <span
+                                    key={data.pozorista[0].pozoristeid}
+                                    className="text-muted"
+                                >
+                                    {data.pozorista[0].naziv_pozorista}
+                                </span>
+                                {data.pozorista.length > 1 && (
+                                    <span className="text-muted more-pozorista">
+                                        {" "}
+                                        + {data.pozorista.length - 1}
+                                    </span>
+                                )}
+                            </>
+                        )}
                     </div>
                     <ul className="list-inline">
                         {data.premijera && (

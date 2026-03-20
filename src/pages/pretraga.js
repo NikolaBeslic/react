e20863735c5af55c48a7723d395f558d4893bead
beslicn <nikola_beslic@yahoo.com>
2026-03-21 00:47:38 +0100

    git react pretraga pagination

--- src/pages/pretraga.js
+++ src/pages/pretraga.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import SearchHeader from "../components/post/post-format/elements/meta/SearchHeader";
 import { useRouter } from "next/router";
 import axiosClient from "../utils/axios";
@@ -13,6 +13,7 @@ const PretragaPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const [searchResults, setSearchResults] = useState([]);
+    const [meta, setMeta] = useState(null);
 
     useEffect(() => {
         setSearchResults([]);
@@ -31,13 +32,19 @@ const PretragaPage = () => {
                     `/pretraga?query=${query}&page=${currentPage}`,
                 );
 
+                console.log(res.data);
+
                 if (currentPage === 1) {
                     setSearchResults(res.data.data || []);
                 } else {
                     setSearchResults((prev) => [...prev, ...res.data.data]);
                 }
 
-                setTotalPages(res.data.last_page || 1);
+                setMeta({
+                    currentPage: res.data.current_page,
+                    lastPage: res.data.last_page,
+                    total: res.data.total,
+                });
             } catch (error) {
                 console.error("Failed to search", error);
                 toast.error("Neka greška se dogodila");
@@ -48,6 +55,15 @@ const PretragaPage = () => {
 
         fetchResults();
     }, [query, currentPage]);
+
+    const canLoadMore = useMemo(() => {
+        if (!meta) return false;
+        return meta.currentPage < meta.lastPage;
+    }, [meta]);
+
+    const loadMore = () => {
+        if (!canLoadMore) return;
+    };
 
     return (
         <>
@@ -61,36 +77,35 @@ const PretragaPage = () => {
                         />
                     </>
                 )}
-                {!loading &&
-                    (searchResults.length > 0 ? (
-                        <>
-                            <p>
-                                Rezultati pretrage za: <strong>{query}</strong>.
-                                Ukupno {searchResults.length} rezultata.
-                            </p>
-                            <ul>
-                                {searchResults.map((result) => (
-                                    <RezultatiPretrage
-                                        data={result}
-                                        key={result.id}
-                                    />
-                                ))}
-                            </ul>
-                        </>
-                    ) : (
-                        <p>Nema rezultat.</p>
-                    ))}
 
-                {loading &&
-                    searchResults.length > 0 &&
-                    currentPage < totalPages && (
-                        <button
-                            className="btn btn-primary btn-small btn-load-more d-block mx-auto mt-4"
-                            onClick={() => setCurrentPage(currentPage + 1)}
-                        >
-                            Učitaj još
-                        </button>
-                    )}
+                {searchResults.length > 0 ? (
+                    <>
+                        <p>
+                            Rezultati pretrage za: <strong>{query}</strong>.
+                            Priakzano {searchResults.length} od ukupno{" "}
+                            {meta.total} rezultata.
+                        </p>
+                        <ul>
+                            {searchResults.map((result) => (
+                                <RezultatiPretrage
+                                    data={result}
+                                    key={result.id}
+                                />
+                            ))}
+                        </ul>
+                    </>
+                ) : (
+                    !loading && <p>Nema rezultata za traženi pojam.</p>
+                )}
+
+                {canLoadMore && (
+                    <button
+                        className="btn btn-primary btn-small btn-load-more d-block mx-auto mt-4"
+                        onClick={() => setCurrentPage(currentPage + 1)}
+                    >
+                        Učitaj još
+                    </button>
+                )}
             </div>
         </>
     );

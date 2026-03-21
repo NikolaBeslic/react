987025995d2f9f2d7c0a85fed73e39e6d3bd9893
beslicn <nikola_beslic@yahoo.com>
2026-03-21 02:06:55 +0100

    git react pozorista layout and search

--- src/components/post/layout/PozoristaLayout.jsx
+++ src/components/post/layout/PozoristaLayout.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const PozoristaLayout = ({ pozoriste }) => {
     return (
-        <div className="pozorista-layout-wrapper col-lg-3 col-md-4 col-xs-6">
+        <div className="pozorista-layout-wrapper">
             <div className="pozorista-layout-logo">
                 <Link href={`/pozorista/${pozoriste.pozoriste_slug}`}>
                     {pozoriste.logo && (
                         <Image
                             src={pozoriste.logo}
                             alt={pozoriste.naziv_pozorista}
-                            height={100}
+                            height={120}
                             width={120}
                             objectFit="contain"
                         />
--- src/pages/pozorista/index.js
+++ src/pages/pozorista/index.js
@@ -2,18 +2,47 @@ import axiosClient from "../../utils/axios";
 import PozoristaLayout from "../../components/post/layout/PozoristaLayout";
 import PozoristaHeader from "../../components/post/post-format/elements/meta/PozoristaHeader";
 import { withSSRHandler } from "../../utils/withSSRHandler";
+import { Form, InputGroup } from "react-bootstrap";
+import { useState } from "react";
 
 export default function PozoristaPage({ pozorista }) {
+    const [pozorja, setPozorja] = useState(pozorista);
+    const [searchInput, setSearchInput] = useState("");
+    const handleSearch = (event) => {
+        setSearchInput(event.target.value);
+        setPozorja(
+            pozorista.filter((item) =>
+                item.naziv_pozorista
+                    .toLowerCase()
+                    .includes(event.target.value.toLowerCase()),
+            ),
+        );
+    };
+
     return (
         <>
             <div className="random-posts section-gap">
-                <div className="axil-content row">
-                    {pozorista.map((pozoriste) => (
-                        <PozoristaLayout
-                            pozoriste={pozoriste}
-                            key={pozoriste.pozoristeid}
+                <div className="axil-content">
+                    <InputGroup className="mb-5 pozorista-search-input-group">
+                        <InputGroup.Text id="basic-addon1">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </InputGroup.Text>
+                        <Form.Control
+                            placeholder="Pretraži pozorišta"
+                            aria-label="seacrh"
+                            aria-describedby="basic-addon1"
+                            value={searchInput}
+                            onChange={handleSearch}
                         />
-                    ))}
+                    </InputGroup>
+                    <div className="pozorista-grid">
+                        {pozorja?.map((pozoriste) => (
+                            <PozoristaLayout
+                                pozoriste={pozoriste}
+                                key={pozoriste.pozoristeid}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </>

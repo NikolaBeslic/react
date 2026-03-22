d6bb7ce9a1bbf9c2c43cb3a3093a655ceec28033
beslicn <nikola_beslic@yahoo.com>
2026-03-23 00:05:53 +0100

    git react links inside text and social share restyle

--- src/components/post/post-format/elements/SocialShareBottom.jsx
+++ src/components/post/post-format/elements/SocialShareBottom.jsx
@@ -6,9 +6,11 @@ const SocialShareBottom = ({ url, title }) => {
                 <li>
                     <a
                         href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
-                        className="btn bg-color-facebook"
+                        className="btn btn-nofill"
                         target="_blank"
                         rel="nofollow noopener noreferrer"
+                        title="Podeli na Fejsu"
+                        aria-label="Podeli na Fejsu"
                     >
                         <i className="fab fa-facebook-f" />
                     </a>
@@ -16,31 +18,39 @@ const SocialShareBottom = ({ url, title }) => {
                 <li>
                     <a
                         href={`https://wa.me/?text=${title}%20${url}`}
-                        className="btn bg-color-whatsapp"
+                        className="btn btn-nofill"
                         target="_blank"
                         rel="nofollow noopener noreferrer"
+                        title="Pošalji preko Whatsapp-a"
+                        aria-label="Pošalji preko Whatsapp-a"
                     >
                         <i className="fab fa-whatsapp" />
                     </a>
                 </li>
-                <li>
-                    <a
-                        href={`viber://forward?text=${url} `}
-                        className="btn bg-color-viber"
-                        target="_blank"
-                        rel="nofollow noopener noreferrer"
-                    >
-                        <i className="fab fa-viber" />
-                    </a>
-                </li>
+
                 <li>
                     <a
                         href={`https://twitter.com/intent/tweet?url=${url}&text=${title}&via=`}
                         target="_blank"
                         rel="nofollow noopener noreferrer"
-                        className="btn bg-color-twitter "
+                        className="btn btn-nofill"
+                        title="Podeli na Tviteru (X)"
+                        aria-label="Podeli na Tviteru (X)"
                     >
                         <i className="fa-brands fa-x-twitter" />
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href={`viber://forward?text=${url} `}
+                        className="btn btn-nofill"
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                        title="Pošalji preko Viber-a"
+                        aria-label="Pošalji preko Viber-a"
+                    >
+                        <i className="fab fa-viber" />
                     </a>
                 </li>
 
@@ -49,7 +59,9 @@ const SocialShareBottom = ({ url, title }) => {
                         href={`mailto:?subject=${title}&body=${url}`}
                         target="_blank"
                         rel="nofollow noopener noreferrer"
-                        className="btn bg-color-email"
+                        className="btn btn-nofill"
+                        title="Pošalji preko emaila"
+                        aria-label="Pošalji preko emaila"
                     >
                         <i className="fa-regular fa-envelope"></i>
                     </a>

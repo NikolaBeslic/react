7d73ca761fb35a5dcb22c8097fe0651f7badde23
beslicn <nikola_beslic@yahoo.com>
2026-03-24 18:55:57 +0100

    git react category pages meta dana

--- src/components/post/post-format/elements/meta/CategoryHeader.jsx
+++ src/components/post/post-format/elements/meta/CategoryHeader.jsx
@@ -3,7 +3,15 @@ import HeadMeta from "../../../../elements/HeadMeta";
 const CategoryHeader = ({ categoryData }) => {
     return (
         <>
-            <HeadMeta metaTitle={categoryData.display_naziv_kategorije} />
+            <HeadMeta
+                metaTitle={categoryData.seo_title}
+                metaDescription={
+                    categoryData.seo_description ||
+                    "Čitaj tekstove o savremenom pozorištu – vesti, intervjue, recenzije, analize i druge priče sa domaće i regionalne scene."
+                }
+                metaUrl={categoryData.seo_url}
+                metaImage={categoryData.seo_image || "/slike/hup-vizitke.jpg"}
+            />
             {/* Banner Start here  */}
             <div className="banner banner__default bg-grey-light-three">
                 <div className="container">

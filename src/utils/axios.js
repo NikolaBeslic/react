a047fdf20899215e59747830581a2bd29f77109b
beslicn <nikola_beslic@yahoo.com>
2026-03-29 13:31:22 +0200

    git react remove xmlhttprequest

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,6 @@ const axiosClient = axios.create({
     timeout: 30000, // Set a timeout of 10 seconds
     withCredentials: true,
     headers: {
-        "X-Requested-With": "XMLHttpRequest",
         Accept: "application/json",
     },
 });
